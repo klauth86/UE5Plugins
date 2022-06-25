a90904aca80e39fcec86ecce9385adbb0adaec0a
kehel@yandex.ru <kehel@yandex.ru>
2022-06-25 16:33:21 +0300

    InputSMExample: Add empty proj

---
+++ InputSMExample/Source/InputSMExample/InputSMExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InputSMExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INPUTSMEXAMPLE_API AInputSMExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
