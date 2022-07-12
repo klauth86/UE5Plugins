ae68f41426c80ca081e8928bbdbe2b4bf762a8dc
klauth86 <kehel@yandex.ru>
2022-07-12 12:50:51 +0300

    InputSequenceExample: Add blank project and blank plugin

---
+++ InputSequenceExample/Source/InputSequenceExample/InputSequenceExample.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "InputSequenceExample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InputSequenceExample, "InputSequenceExample" );
---
+++ InputSequenceExample/Source/InputSequenceExample/InputSequenceExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InputSequenceExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INPUTSEQUENCEEXAMPLE_API AInputSequenceExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
